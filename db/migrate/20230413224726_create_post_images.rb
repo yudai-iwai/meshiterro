095a4f0fba4e5953a6628c29ab37e617258f8bfb
EC2 Default User <129721056+yudai-iwai@users.noreply.github.com>
2023-04-14 06:19:15 +0000

    [6]

--- db/migrate/20230413224726_create_post_images.rb
+++ db/migrate/20230413224726_create_post_images.rb
@@ -4,7 +4,7 @@ class CreatePostImages < ActiveRecord::Migration[6.1]
 
       t.string :shop_name
       t.text :caption
-      t.integer :user_
+      t.integer :user_id
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -43,7 +43,7 @@ ActiveRecord::Schema.define(version: 2023_04_13_224726) do
   create_table "post_images", force: :cascade do |t|
     t.string "shop_name"
     t.text "caption"
-    t.integer "user_"
+    t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
