3538df598ec8655a49358b0129c41c13d103e8f8
EC2 Default User <129721056+yudai-iwai@users.noreply.github.com>
2023-04-15 05:05:27 +0000

    [9]

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-resources :users, only: [:show, :edit]
-resources :post_images, only: [:new, :create, :index, :show, :destroy]
-devise_for :users
-root to: "homes#top"
-get 'homes/about', as:'about'
+ devise_for :users
+ root to: "homes#top"
+ get 'homes/about', as:'about'
+ resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+  resources :post_comments, only: [:create, :destroy]
+ end
+ resources :users, only: [:show, :edit, :update]
 end
